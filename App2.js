85d8e3fd4e0a3c5f7751deca37a9094d1c72c9af
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-01-17 21:05:39 +0530

    basics-react

--- App2.js
+++ App2.js
@@ -1,7 +1,23 @@
-const heading = React.createElement(
-  "h1",
-  { id: root },
-  "Hello world from React!"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: root },
+// //   "Hello world from React!"
+// // );
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // const parent = React.createElement("div", { id: "parent" }, [
+// //   React.createElement("div", { id: "child" }, [
+// //     React.createElement("h1", {}, "I'm a h1 tag"),
+// //     React.createElement("h2", {}, "I'm h2"),
+// //   ]),
+// //   React.createElement("div", { id: "child2" }, [
+// //     React.createElement("h1", {}, "I'm h1"),
+// //     React.createElement("h2", {}, "I'm h2"),
+// //   ]),
+// // ]);
+
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(parent);
