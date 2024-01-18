e726dea4676f9a4f9a033a805e47373541235e26
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-01-18 21:31:52 +0530

    react-basics-practice

--- App1.js
+++ App1.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// React code
-const heading = React.createElement("h1", { id: "heading" }, "Namaste-react");
-console.log(heading);
+// React Element
+// const heading = React.createElement("h1", { id: "heading" }, "React");
+// console.log(heading);
 
-//JSX
-const jsxHeading = <h1 id="heading">Namaste-React</h1>;
-console.log(jsxHeading);
+// JSX
+// const heading = <h1 id="heading">hey buddy!</h1>;
 
+// console.log(heading);
+
+//React Componets
+const elem = <span>React-element</span>;
+const Title = () => (
+  <h1 className="head">
+    {elem}
+    Namste react
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {/* <title></title> ={<title />}  {Title()}*/}
+    {Title()}
+    <h1 className="heading">Namste-React functional component</h1>
+  </div>
+);
+
+// Render
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
--- App2.js
+++ App2.js
@@ -21,3 +21,18 @@
 
 // // const root = ReactDOM.createRoot(document.getElementById("root"));
 // // root.render(parent);
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// React code
+// const heading = React.createElement("h1", { id: "heading" }, "React");
+// console.log(heading);
+
+// JSX
+const jsxHeading = <h1 id="heading">hey buddy!</h1>;
+
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
